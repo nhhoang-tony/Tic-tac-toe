08ea09799e5c7750e68286cff8395ca0e25717e2
Tony Nguyen <t.nguyen@vbreathe.com>
2024-01-23 00:36:13 +1000

    Add Vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <App />
+    <Analytics></Analytics>
   </React.StrictMode>
 );
